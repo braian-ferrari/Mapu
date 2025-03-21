4fa1e06d55f474cf96b4a19857d49818e497e583
BraianFerrari <braian.ferrari.bf@gmail.com>
2025-03-21 00:02:44 -0300

    mejore el responsive

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+if (window.innerWidth > 768) { // Solo en pantallas más grandes
+    window.addEventListener('scroll', function() {
+      const nav = document.querySelector('nav.navbar');
+      if (window.scrollY > 50) {
+        nav.style.backgroundColor = 'rgba(0, 0, 0, 0.9)';
+      } else {
+        nav.style.backgroundColor = 'rgba(0, 0, 0, 0.6)';
+      }
+    });
+  }
 
-  window.addEventListener('scroll', function() {
-    const nav = document.querySelector('nav.navbar');
-    if (window.scrollY > 50) {
-      nav.style.backgroundColor = 'rgba(0, 0, 0, 0.9)';
-    } else {
-      nav.style.backgroundColor = 'rgba(0, 0, 0, 0.6)';
-    }
-  });
 
+  
