d3979a49532cb462c293436f75d9ac849fe05747
BraianFerrari <braian.ferrari.bf@gmail.com>
2025-03-27 21:03:02 -0300

    nuevo repo

---
+++ index.js
@@ -0,0 +1,13 @@
+if (window.innerWidth > 768) { // Solo en pantallas más grandes
+    window.addEventListener('scroll', function() {
+      const nav = document.querySelector('nav.navbar');
+      if (window.scrollY > 50) {
+        nav.style.backgroundColor = 'rgba(0, 0, 0, 0.9)';
+      } else {
+        nav.style.backgroundColor = 'rgba(0, 0, 0, 0.6)';
+      }
+    });
+  }
+
+
+  
