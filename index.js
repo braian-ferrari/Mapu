31c79b0e018c0e731830f6ec89429202be99119e
BraianFerrari <braian.ferrari.bf@gmail.com>
2025-04-11 15:04:44 -0300

    nav responsive

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-if (window.innerWidth > 768) { // Solo en pantallas más grandes
-    window.addEventListener('scroll', function() {
-      const nav = document.querySelector('nav.navbar');
-      if (window.scrollY > 50) {
-        nav.style.backgroundColor = 'rgba(0, 0, 0, 0.9)';
-      } else {
-        nav.style.backgroundColor = 'rgba(0, 0, 0, 0.6)';
+document.addEventListener('DOMContentLoaded', function () {
+  const navbarCollapse = document.getElementById('navbarMenuMobile');
+  const links = navbarCollapse.querySelectorAll('.nav-link, .btn');
+
+  links.forEach(link => {
+    link.addEventListener('click', () => {
+      const bsCollapse = bootstrap.Collapse.getInstance(navbarCollapse);
+      if (bsCollapse) {
+        bsCollapse.hide();
       }
     });
-  }
+  });
+});
 
-
-  
